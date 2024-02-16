1c0a487edaf3fdbd87a9e689361fa3ecd3f5d2f6
Mohammad Rafiqul Islam Hridoy <rafiqulislam11002@gmail.com>
2024-02-16 20:36:22 +0600

    all pages are created

---
+++ client/src/app/(homelayout)/our-reviews/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const OurReviews = () => {
+  return (
+    <div>
+      <h2>Here you can see our clients says</h2>
+    </div>
+  );
+};
+
+export default OurReviews;
