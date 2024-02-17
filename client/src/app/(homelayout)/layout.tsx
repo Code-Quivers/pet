5e33d2e00a5e1bf50c244d44670245a64a377dca
Mohammad Rafiqul Islam Hridoy <rafiqulislam11002@gmail.com>
2024-02-17 16:05:26 +0600

    footer added

--- client/src/app/(homelayout)/layout.tsx
+++ client/src/app/(homelayout)/layout.tsx
@@ -1,16 +1,21 @@
+import Footer from "@/components/Footer/Footer";
 import Navbar from "@/components/Navbar/Navbar";
 import React from "react";
 
 const layout = ({ children }: any) => {
   return (
     <div>
-      <div>
+      <div className="sticky top-0 z-50">
         <Navbar />
       </div>
       <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
         {children}
       </div>
-      <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">Footer</div>
+      <div className="bg-primary">
+        <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 };
--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -8,19 +8,25 @@ import { IoCartOutline } from "react-icons/io5";
 import { CiSearch } from "react-icons/ci";
 import NavbarDrawer from "./NavbarDrawer";
 import Cart from "../ProductsPage/Cart/Cart";
+import SearchModalDrawer from "./SearchModalDrawer";
 
 const Navbar = () => {
   const [open, setOpen] = useState(false);
   const [cartOpen, setCartOpen] = useState(false);
+  const [searchOpen, setSearchOpen] = useState(false);
   return (
     <header className="bg-primary">
       <NavbarDrawer open={open} setOpen={setOpen} />
       <Cart cartOpen={cartOpen} setCartOpen={setCartOpen} />
+      <SearchModalDrawer
+        searchOpen={searchOpen}
+        setSearchOpen={setSearchOpen}
+      />
       <div className="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <div className="flex-1 md:flex md:items-center md:gap-12">
             <Image
-              className="w-52 object-cover"
+              className="w-60 object-fill"
               src={logo}
               width={100}
               height={100}
@@ -30,7 +36,10 @@ const Navbar = () => {
           <div className="md:flex md:items-center md:gap-12">
             <div className="flex items-center gap-4">
               <div className="block">
-                <button className="rounded py-2 transition text-white text-3xl">
+                <button
+                  onClick={() => setSearchOpen(true)}
+                  className="rounded py-2 transition text-white text-3xl"
+                >
                   <CiSearch />
                 </button>
               </div>
