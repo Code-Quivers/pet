19c1b266b9c5bd22f13d8699ac03f36a40090a9e
salimalsazu <sazusalim@gmail.com>
2024-03-02 02:22:45 +0600

    Pet Api

--- server/src/app/modules/Pet/pet.routes.ts
+++ server/src/app/modules/Pet/pet.routes.ts
@@ -3,22 +3,30 @@ import express, { NextFunction, Request, Response } from 'express';
 import { FileUploadHelper } from '../../../helpers/FileUploadHelper';
 import auth from '../../middlewares/auth';
 import { UserRoles } from '@prisma/client';
-import { ProductController } from './pet.controller';
+import { PetController } from './pet.controller';
 import { PetValidation } from './pet.validation';
+import { IRequestUser } from './pet.interface';
 
 const router = express.Router();
 
 // ! Create New List ------------------------------->>>
 
-router.post('/', FileUploadHelper.uploadProductImage.single('file'), (req: Request, res: Response, next: NextFunction) => {
-  req.body = PetValidation.addPet.parse(JSON.parse(req.body.data));
-  return ProductController.addProductController(req, res, next);
-});
+router.post(
+  '/',
+
+  auth(UserRoles.USER, UserRoles.ADMIN, UserRoles.SUPERADMIN),
+  FileUploadHelper.uploadProductImage.single('file'),
+  (req: Request, res: Response, next: NextFunction) => {
+    req.body = PetValidation.addPet.parse(JSON.parse(req.body.data));
+
+    return PetController.addPetController(req, res, next);
+  }
+);
 
 // ! Get all List----------------------------------->>>
-router.get('/', ProductController.getProductController);
+router.get('/', PetController.getPetController);
 
-router.get('/tag/:productId', ProductController.getSingleProduct);
+router.get('/tag/:productId', PetController.getSinglePet);
 
 router.patch(
   '/:productId',
@@ -27,14 +35,14 @@ router.patch(
   (req: Request, res: Response, next: NextFunction) => {
     req.body = ProductZodValidation.editProduct.parse(JSON.parse(req.body.data));
     console.log(req.body.data, 'body.....');
-    return ProductController.updateProduct(req, res, next);
+    return PetController.updatePet(req, res, next);
   }
 );
 
 router.delete(
   '/:productId',
   // auth(UserRoles.USER, UserRoles.ADMIN, UserRoles.SUPERADMIN),
-  ProductController.deleteProduct
+  PetController.deletePet
 );
 
-export const ProductRoutes = router;
+export const PetRoutes = router;
--- server/src/app/modules/Pet/pet.utils.ts
+++ server/src/app/modules/Pet/pet.utils.ts
@@ -1,13 +1,12 @@
 import httpStatus from 'http-status';
 import ApiError from '../../../errors/ApiError';
 
-import { IPetRequest } from './pet.interface';
+import { IPetRequest, IRequestUser } from './pet.interface';
 import prisma from '../../../shared/prisma';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export const PetValidation = async (data: IPetRequest) => {
-
-  if(!data.productId) {
+export const PetValidation = async (data: IPetRequest, userId: IRequestUser) => {
+  if (!data.productId) {
     throw new ApiError(httpStatus.BAD_REQUEST, 'Pet name is required');
   }
 
@@ -15,12 +14,38 @@ export const PetValidation = async (data: IPetRequest) => {
     where: {
       productId: data.productId,
     },
-  })
+  });
 
   if (!isProductExist) {
     throw new ApiError(httpStatus.NOT_FOUND, 'Product Not Found!!');
   }
 
+  if (!userId) {
+    throw new ApiError(httpStatus.BAD_REQUEST, 'User Id is required');
+  }
 
+  const petAssign = await prisma.pet.findFirst({
+    where: {
+      userId: userId.userId,
+      productId: data.productId,
+    },
+  });
 
-}
+  if (petAssign) {
+    throw new ApiError(httpStatus.CONFLICT, 'Pet Already Assigned with User ');
+  }
+
+  const petAssignToOtherUser = await prisma.pet.findFirst({
+    where: {
+      NOT: {
+        userId: userId.userId,
+      },
+      productId: data.productId,
+    },
+  });
+
+  if (petAssignToOtherUser) {
+    throw new ApiError(httpStatus.CONFLICT, 'Pet Already Assigned to a different user');
+  }
+
+};
