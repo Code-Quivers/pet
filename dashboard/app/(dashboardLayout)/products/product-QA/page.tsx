1111d0054353ec0f67e364ad248716d8b1ffc3fb
salimalsazu <sazusalim@gmail.com>
2024-03-09 20:45:20 +0600

    [PENDING] QA Table

--- dashboard/app/(dashboardLayout)/products/product-QA/page.tsx
+++ dashboard/app/(dashboardLayout)/products/product-QA/page.tsx
@@ -1,10 +1,14 @@
 import AddProductQAForm from "@/components/products/forms/AddProductQAForm";
+import ProductQATable from "@/components/products/tables/ProductQATable";
 
 const ProductQAPage = () => {
   return (
     <div>
       <div>
         <AddProductQAForm />
+      </div>
+      <div>
+        <ProductQATable />
       </div>
     </div>
   );
