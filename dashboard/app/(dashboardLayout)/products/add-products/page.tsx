ce2ee38203ed3b724f584c80abaad693563d08c0
shourovr82 <shafinur512@gmail.com>
2024-03-26 12:53:20 +0600

    [DONE] product variant image upload bug fixed

--- dashboard/app/(dashboardLayout)/products/add-products/page.tsx
+++ dashboard/app/(dashboardLayout)/products/add-products/page.tsx
@@ -1,4 +1,9 @@
 import AddProductsSection from "@/components/products/addProducts/AddProducts";
+import { Metadata } from "next";
+
+export const metadata:Metadata ={
+  title:"Add New Product"
+}
 
 const AddProductsPage = () => {
   return (
