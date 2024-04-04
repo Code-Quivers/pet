22d7c68f93d984fb409d1270196f348a21d341f4
mostafizcw <mostafizur.rahaman.cw@gmail.com>
2024-04-04 16:19:05 +0600

    [Done] product design changed

--- client/src/components/HomePage/ProductsSection.tsx
+++ client/src/components/HomePage/ProductsSection.tsx
@@ -18,6 +18,10 @@ const ProductsSection = () => {
   const dispatch = useDispatch();
   const toaster = useToaster();
   const [start, setStart] = useState(false);
+  const [selectedColor, setSelectedColor] = useState({
+    productId: null,
+    index: 0,
+  });
   const [productId, setProductId] = useState(null);
   console.log(products, "productList");
   const message = (
@@ -45,7 +49,7 @@ const ProductsSection = () => {
     }, 1000);
   };
   return (
-    <section className="flex items-center bg-gray-100 mt-10 py-10">
+    <section className="flex items-center mt-10 py-10">
       <div className="p-4 mx-auto max-w-7xl">
         <div className="max-w-xl mx-auto">
           <div className="text-center ">
@@ -70,31 +74,65 @@ const ProductsSection = () => {
             </p>
           </div>
         </div>
-        <div className="grid grid-cols-1 gap-4 lg:gap-4 sm:gap-4 md:grid-cols-3">
+        <div className="grid grid-cols-1 gap-4 lg:gap-4 sm:gap-4 md:grid-cols-3 max-w-4xl">
           {products?.map((product: any) => (
-            <div
-              key={product.id}
-              className="mt-56 bg-white rounded shadow md:w-72"
-            >
-              <div className="relative z-20 p-6 group">
-                <div className="relative block h-64 mb-4 -mt-56 overflow-hidden rounded -top-full ">
-                  <div className="relative w-full h-full">
+            <div key={product.id} className="shadow-lg">
+              {console.log(product, "product")}
+              <div className="">
+                <Link href={`/shop/single-product/${product?.productId}`}>
+                  <div className="w-full h-full">
                     <Image
                       width={200}
                       height={200}
-                      src={`${fileUrlKey()}/${product?.featuredImage}`}
+                      src={
+                        selectedColor?.productId == product?.productId
+                          ? `${fileUrlKey()}/${
+                              product?.productVariations[selectedColor.index]
+                                ?.image
+                            }`
+                          : `${fileUrlKey()}/${product?.featuredImage}`
+                      }
                       alt={product.productName}
-                      className="absolute inset-0 h-full w-full object-cover opacity-100 group-hover:opacity-0 transition-all group-hover:scale-110"
+                      className="h-full w-full object-cover opacity-100 group-hover:opacity-0 transition-all group-hover:scale-110"
                     />
-                    <Image
-                      width={200}
-                      height={200}
-                      src={`${fileUrlKey()}/${product?.featuredImage}`}
-                      alt={product.productName}
-                      className="absolute inset-0 h-full w-full object-cover opacity-0 group-hover:opacity-100 transition-all group-hover:scale-110"
-                    />
+                    {/* <Image
+                    width={200}
+                    height={200}
+                    src={`${fileUrlKey()}/${product?.featuredImage}`}
+                    alt={product.productName}
+                    className=" h-full w-full object-cover opacity-0 group-hover:opacity-100 transition-all group-hover:scale-110"
+                  /> */}
                   </div>
-                  <div className="absolute flex flex-col top-4 right-4">
+                  <div className="px-4 py-2">
+                    <h2 className=" text-base font-bold text-black ">
+                      {product?.productName}
+                    </h2>
+                    <p className="text-base pt-1 font-bold text-primary">
+                      <span>${product?.productPrice?.toFixed(2)}</span>
+                    </p>
+                  </div>
+                </Link>
+                <div className="px-4 pb-2">
+                  {product?.productVariations?.map(
+                    (variation: any, index: number) => (
+                      <button
+                        onClick={() =>
+                          setSelectedColor({
+                            productId: product.productId,
+                            index,
+                          })
+                        }
+                        style={{
+                          backgroundColor: `${variation?.color?.code}`,
+                        }}
+                        key={variation?.variantId}
+                        className={`w-6 h-6 rounded-full mr-2`}
+                      ></button>
+                    )
+                  )}
+                </div>
+
+                {/* <div className="absolute flex flex-col top-4 right-4">
                     <a href="#" className="flex items-center">
                       <div className="relative flex items-center justify-center p-3 mb-3 transition-all translate-x-20 bg-white rounded group-hover:translate-x-0 wishlist hover:bg-blue-200    group">
                         <svg
@@ -110,11 +148,7 @@ const ProductsSection = () => {
                       </div>
                     </a>
                     {start && productId === product.productId ? (
-                      <button
-                        className="flex items-center"
-
-                        // onClick={() => dispatch(addToCart(product) as any)}
-                      >
+                      <button className="flex items-center">
                         <div
                           className="relative flex items-center justify-center p-3 mb-3 transition-all translate-x-20 bg-white rounded 
                            group-hover:translate-x-0 wishlist hover:bg-blue-200 group"
@@ -126,86 +160,25 @@ const ProductsSection = () => {
                       <button
                         className="flex items-center"
                         onClick={() => addToCartHandler(product)}
-                        // onClick={() => dispatch(addToCart(product) as any)}
                       >
                         <div
                           className="relative flex items-center justify-center p-3 mb-3 transition-all translate-x-20 bg-white rounded 
                        group-hover:translate-x-0 wishlist hover:bg-blue-200 group"
                         >
                           <CartIcon />
-                          {/* {start && productId === product.productId ? (
-                          <Loader size="sm" />
-                        ) : (
-                          <CartIcon />
-                        )} */}
                         </div>
                       </button>
                     )}
-                  </div>
-                </div>
-                <Link href={`/shop/single-product/${product?.productId}`}>
+                  </div> */}
+
+                {/* <Link href={`/shop/single-product/${product?.productId}`}>
                   <h2 className="mb-2 text-xl font-bold text-black  ">
                     {product?.productName}
                   </h2>
                   <p className="mb-3 text-lg font-bold text-primary">
                     <span>${product?.productPrice?.toFixed(2)}</span>
-                    {/* <span className="text-xs font-semibold text-gray-400 line-through ">
-                    $200.00
-                  </span> */}
                   </p>
-                  {/* <div className="flex gap-1 text-orange-400">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-star-fill"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                    </svg>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-star-fill"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                    </svg>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-star-fill"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                    </svg>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-star-fill"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z" />
-                    </svg>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-star"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z" />
-                    </svg>
-                  </div> */}
-                </Link>
+                </Link> */}
               </div>
             </div>
           ))}
