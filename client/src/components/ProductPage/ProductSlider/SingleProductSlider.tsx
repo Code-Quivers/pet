b90b642542d8a493e18859aad8a04d58cc5432ee
mostafizcw <mostafizur.rahaman.cw@gmail.com>
2024-04-05 22:45:26 +0600

    [DONE] placeholder and quantity done

--- client/src/components/ProductPage/ProductSlider/SingleProductSlider.tsx
+++ client/src/components/ProductPage/ProductSlider/SingleProductSlider.tsx
@@ -9,11 +9,25 @@ type PropType = {
   slides: any[];
   options?: EmblaOptionsType;
   selectColorName: string | null;
+  isLoading: boolean;
+  isFetching: boolean;
   setSelectColorName: (color: string | null) => void;
 };
+const sliderThumbLoader = Array.from({ length: 3 }).map((_, index) => (
+  <div className="embla-thumbs__container" key={index}>
+    <div key={index} className="animate-pulse bg-gray-300 mr-2"></div>
+  </div>
+));
 
 const SingleProductSlider: React.FC<PropType> = (props) => {
-  const { slides, options, selectColorName, setSelectColorName } = props;
+  const {
+    slides,
+    options,
+    selectColorName,
+    setSelectColorName,
+    isFetching,
+    isLoading,
+  } = props;
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [emblaMainRef, emblaMainApi] = useEmblaCarousel(options);
   const [emblaThumbsRef, emblaThumbsApi] = useEmblaCarousel({
@@ -64,37 +78,45 @@ const SingleProductSlider: React.FC<PropType> = (props) => {
     <div className="embla">
       <div className="embla__viewport" ref={emblaMainRef}>
         <div className="embla__container">
-          {slides?.map((slide) => (
-            <div className="embla__slide" key={slide.id}>
-              <div className="embla__slide__number">
-                <Image
-                  src={`${fileUrlKey()}/${slide?.src}`}
-                  alt="Product Image"
-                  className="object-cover"
-                  width={300}
-                  height={300}
-                />
-              </div>
+          {isLoading || isFetching ? (
+            <div className="animate-pulse embla__slide bg-gray-300">
+              <div className="embla__slide__number"></div>
             </div>
-          ))}
+          ) : (
+            slides?.map((slide) => (
+              <div className="embla__slide" key={slide.id}>
+                <div className="embla__slide__number">
+                  <Image
+                    src={`${fileUrlKey()}/${slide?.src}`}
+                    alt="Product Image"
+                    className="object-cover"
+                    width={300}
+                    height={300}
+                  />
+                </div>
+              </div>
+            ))
+          )}
         </div>
       </div>
 
       <div className="embla-thumbs">
         <div className="embla-thumbs__viewport" ref={emblaThumbsRef}>
           <div className="embla-thumbs__container">
-            {slides?.map((slide, index) => (
-              <CarouselThumbsButton
-                key={slide.id}
-                onClick={() => {
-                  setSelectColorName(null);
-                  onThumbClick(index);
-                }}
-                // onClick={() => handleVariantClick(slide.color)}
-                selected={index === selectedIndex}
-                slide={slide}
-              />
-            ))}
+            {isLoading || isFetching
+              ? sliderThumbLoader
+              : slides?.map((slide, index) => (
+                  <CarouselThumbsButton
+                    key={slide.id}
+                    onClick={() => {
+                      setSelectColorName(null);
+                      onThumbClick(index);
+                    }}
+                    // onClick={() => handleVariantClick(slide.color)}
+                    selected={index === selectedIndex}
+                    slide={slide}
+                  />
+                ))}
           </div>
         </div>
       </div>
