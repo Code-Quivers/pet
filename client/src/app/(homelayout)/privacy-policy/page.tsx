d81e4e9a7382401f624ced5444f489a49c4a0699
salimalsazu <sazusalim@gmail.com>
2024-05-22 23:08:35 +0600

    [DONE] Privacy Policy

--- client/src/app/(homelayout)/privacy-policy/page.tsx
+++ client/src/app/(homelayout)/privacy-policy/page.tsx
@@ -1,3 +1,4 @@
+import { PrivacyPolicyAllData } from "@/components/privacy-policy/PrivacyPolicyData";
 import React from "react";
 
 const PrivacyPolicy = () => {
@@ -7,12 +8,34 @@ const PrivacyPolicy = () => {
         <h2 className="font-bold text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
           Privacy Policy
         </h2>
+        <p className="text-gray-500 font-bold">Last Updated: May 22, 2024 </p>
+      </div>
 
-        <p className="max-w-[85%] leading-normal text-muted-foreground sm:text-lg sm:leading-7">
-          The product can personalize user experiences by understanding
-          individual preferences and tailoring recommendations or content based
-          on user behavior and historical data.
+      <div className="mt-5">
+        <p className=" text-muted-foreground text-2xl leading-7 uppercase font-bold">
+          At E.T. Phone Home Bands, we value your privacy and are committed to
+          protecting your personal information. This Privacy Policy explains how
+          we collect, use, share, and protect your information when you visit
+          our website or make a purchase.
         </p>
+      </div>
+
+      {/* Data mapping */}
+      <div className="mt-5">
+        {PrivacyPolicyAllData.map((data: any, index: any) => (
+          <div key={index}>
+            <div className="my-3">
+              <h1 className="text-xl font-bold">{data.title}</h1>
+              <ul className="text-lg flex flex-col gap-2 mt-2">
+                {data.content.map((content: any, index: any) => (
+                  <li className="indent-10" key={index}>
+                    - {content}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        ))}
       </div>
     </div>
   );
