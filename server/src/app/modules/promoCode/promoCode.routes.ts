68fea6a2f8754f13e2c4db4676a0587fdf3a7fc5
salimalsazu <sazusalim@gmail.com>
2024-05-23 01:19:13 +0600

    [DONE] Promo check

--- server/src/app/modules/promoCode/promoCode.routes.ts
+++ server/src/app/modules/promoCode/promoCode.routes.ts
@@ -8,13 +8,15 @@ import { PromoCodeController } from './promoCode.controller';
 const router = express.Router();
 
 // ! Get all List----------------------------------->>>
-router.post("/create", auth(UserRoles.SUPERADMIN, UserRoles.ADMIN), PromoCodeController.createPromotion)
-router.patch("/:promotionId", auth(UserRoles.SUPERADMIN, UserRoles.ADMIN), PromoCodeController.updatePromotion)
-router.delete("/delete/:promotionId", auth(UserRoles.SUPERADMIN, UserRoles.ADMIN), PromoCodeController.deletePromotion)
-router.get("/", auth(UserRoles.SUPERADMIN, UserRoles.ADMIN), PromoCodeController.getPromotions)
-router.get("/check/:promoCode", auth(UserRoles.SUPERADMIN, UserRoles.ADMIN), PromoCodeController.isExist)
-router.post('/apply-promotion-code/:promoCode',
-    // auth(UserRoles.USER), 
-    PromoCodeController.applyPromoCode)
+router.post('/create', auth(UserRoles.SUPERADMIN, UserRoles.ADMIN), PromoCodeController.createPromotion);
+router.patch('/:promotionId', auth(UserRoles.SUPERADMIN, UserRoles.ADMIN), PromoCodeController.updatePromotion);
+router.delete('/delete/:promotionId', auth(UserRoles.SUPERADMIN, UserRoles.ADMIN), PromoCodeController.deletePromotion);
+router.get('/', auth(UserRoles.SUPERADMIN, UserRoles.ADMIN), PromoCodeController.getPromotions);
+router.get('/check/:promoCode', PromoCodeController.isExist);
+router.post(
+  '/apply-promotion-code/:promoCode',
+  // auth(UserRoles.USER),
+  PromoCodeController.applyPromoCode
+);
 
 export const PromoCodeRoutes = router;
