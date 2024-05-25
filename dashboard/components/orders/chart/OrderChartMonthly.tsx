79f2ce81a647b96c6add66dc465788cdb7b29bd7
salimalsazu <sazusalim@gmail.com>
2024-05-26 02:01:00 +0600

    [PENDING] Order

---
+++ dashboard/components/orders/chart/OrderChartMonthly.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top" as const,
+    },
+    title: {
+      display: true,
+      text: "Chart.js Bar Chart",
+    },
+  },
+};
+
+const labels = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+export const data = {
+  labels,
+  datasets: [
+    {
+      label: "Dataset 1",
+      data: [300, 450, 200, 500, 700, 400, 600],
+      backgroundColor: "rgba(255, 99, 132, 0.5)",
+    },
+  ],
+};
