9c11ad0a543d653798ac7ae77cc4998e8df54808
shourovr82 <shafinur512@gmail.com>
2024-06-02 01:36:57 +0600

    [DONE] IMAGE BUG FIXED

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -1,16 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    // domains: [
-    //   "localhost",
-    //   "images.unsplash.com",
-    //   "cdn.pixabay.com",
-    //   "readymadeui.com",
-    //   "tailwindcss.com",
-    // ],
     remotePatterns: [
       {
         hostname: "localhost",
+        port: "9000",
+        protocol: "http",
+        pathname: "/**",
+      },
+      {
+        hostname: "http://85.239.232.185",
         port: "9000",
         protocol: "http",
         pathname: "/**",
@@ -20,3 +19,6 @@ const nextConfig = {
 };
 
 export default nextConfig;
+// NEXT_PUBLIC_API_BASE_URL=http://85.239.232.185:9000/backend/api/v1
+// NEXT_PUBLIC_API_AUTH_KEY=et_account_auth
+// NEXT_PUBLIC_FILE_URL_KEY=http://85.239.232.185:9000/backend/api/v1
